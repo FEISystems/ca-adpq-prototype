e870ff964b6e47c16c88bd64c74e9c12287777cc
Terry.Boswell <trboswell74@comcast.net>
2017-02-13 14:40:00 -0500

    Basic Configuration Implementation

--- ca_proto/ca_service/Services/UserService.cs
+++ ca_proto/ca_service/Services/UserService.cs
@@ -5,31 +5,42 @@ using System.Threading.Tasks;
 using ca_service.Interfaces;
 using ca_service.Database;
 using ca_service.Entities;
+using Microsoft.Extensions.Configuration;
+
 namespace ca_service.Services
 {
-    public class UserService : IUserService
+    public class UserService : IUserService, IDisposable
     {
+        private readonly Connection db;
+        public UserService(IConfiguration configuration)
+        {
+            db = new Connection(configuration);
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
 
         public List<User> GetUsers()
         {
             var users = new List<User>();
-            using (var db = new Connection(string.Empty))
-            {
-                var cmd = db.connection.CreateCommand();
-                cmd.CommandText = "Select UserId, Name, Password from users";
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        users.Add(new User((int)reader["UserId"])
-                        {
-                            Name = reader["Name"].ToString(),
-                            Password = reader["Password"].ToString()
 
-                        });                        
-                    }
+            var cmd = db.connection.CreateCommand();
+            cmd.CommandText = "Select Id, Name, Password from users";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    users.Add(new User((int)reader["Id"])
+                    {
+                        Name = reader["Name"].ToString(),
+                        Password = reader["Password"].ToString()
+
+                    });
                 }
             }
+
             return users;
         }
     }
