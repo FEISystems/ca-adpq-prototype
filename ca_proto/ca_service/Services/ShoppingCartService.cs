610b5ec215545d709285be1a68976f9a9fd761ae
jonas.bush <jonas.bush@feisystems.com>
2017-02-14 15:20:55 -0700

    Add stub for shopping cart, update data entities.

---
+++ ca_proto/ca_service/Services/ShoppingCartService.cs
@@ -0,0 +1,21 @@
+﻿using ca_service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ca_service.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        public ShoppingCartService()
+        {
+
+        }
+
+        public void ClearShoppingCart(int shoppingCartId)
+        {
+            //todo
+        }
+    }
+}
