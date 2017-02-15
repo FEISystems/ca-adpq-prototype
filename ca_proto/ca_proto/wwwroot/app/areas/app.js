20d3870baca827fb8958ecca656fb3f6dc9beb4c
Luis Najera <luis.najera@feisystems.com>
2017-02-15 15:54:43 -0500

    Initial Check-In of UI

---
+++ ca_proto/ca_proto/wwwroot/app/areas/app.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    "use strict";
+    var module = angular.module("caWebApp", ["ngComponentRouter", 'ngAnimate']);
+    window.document.title = 'CA Prototype';
+    module.value("$routerRootComponent", "caWebApp");
+
+    var controller = function (roleService) {
+        var model = this;
+        model.$onInit = function () {
+            //Do Something
+        }
+    };
+
+    module.component("caWebApp", {
+        controller: controller,
+        bindings: {
+            userRole: "@"
+        },
+        template: '<master-layout>' +
+                        '<ng-outlet style="display: block"></ng-outlet>' +
+                    '</master-layout>'
+            ,
+        $routeConfig: [
+            { path: '/home', component: 'home', name: 'Home' },
+            { path: '/adminlogin', component: 'adminLogin', name: 'Admin Login' },
+            { path: '/aboutus', component: 'aboutUs', name: 'About Us' },
+            { path: '/help', component: 'help', name: 'Help' },
+            { path: '/cart', component: 'cart', name: 'Cart' },
+            { path: '/compare', component: 'compareList', name: 'Compare List' },
+            { path: '/advancedsearch', component: 'advancedSearch', name: 'Advanced Search' },
+            { path: '/**', redirectTo: ['Home'] }
+    ]
+    });
+
+}());
+
+
+function getSrv(name, element) {
+    element = element || '*[ng-app]';
+    return angular.element(element).injector().get(name);
+}
+
+//Dealing with IE compatibility
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+if (!String.prototype.toTitleCase) {
+    String.prototype.toTitleCase = function(){
+      return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+}
+
---
+++ ca_proto/ca_proto/wwwroot/app/areas/registereduser/accountinformation/accountinformation.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+    var module = angular.module("caWebApp");
+
+    var controller = function ($scope, $location) {
+        var model = this;
+        model.provider = {};
+        model.title = "Account Information";
+
+    };
+
+    module.component("accountInformation", {
+        templateUrl: "app/areas/registereduser/accountinformation/accountinformation.html",
+        controllerAs: "model",
+        controller: ["$scope", "$location", controller]
+
+    });
+}())
