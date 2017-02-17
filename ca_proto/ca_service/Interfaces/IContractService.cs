d31d39c5d121768379b0031f9be2c999c58c793d
David.lorek <David.lorek@DAVIDLOREKLT>
2017-02-17 09:35:47 -0500

    Added controller to implement and test contract capabilities.

---
+++ ca_proto/ca_service/Interfaces/IContractService.cs
@@ -0,0 +1,19 @@
+﻿using ca_service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ca_service.Interfaces
+{
+    public interface IContractService
+    {
+        void Add(Contract contract);
+        void Update(Contract contract);
+        Contract Get(int id);
+        void Delete(int id);
+        IEnumerable<Contract> Fetch(int start, int count);
+        string OrderColumnName { get; set; }
+        bool OrderAscending { get; set; }
+    }
+}
---
+++ ca_proto/ca_service/Repositories/ContractRepository.cs
@@ -0,0 +1,17 @@
+﻿using ca_service.Entities;
+using ca_service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ca_service.Repositories
+{
+    public class ContractRepository : EntityRepository<Contract>, IContractRepository
+    {
+        public ContractRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
