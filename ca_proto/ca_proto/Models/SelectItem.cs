bb429e9ea6a6e2226611be8a96d0c2efb7c87c82
David.lorek <David.lorek@DAVIDLOREKLT>
2017-02-17 09:08:48 -0500

    Separated Contract and Category classes into their own files. Added text file with contract table create script.

---
+++ ca_proto/ca_proto/Models/SelectItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ca_proto.Models
+{
+    public class SelectItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public static IEnumerable<SelectItem> FromEnum<EnumType>()
+        {
+            foreach (var item in Enum.GetValues(typeof(EnumType)))
+            {
+                yield return new SelectItem { Id = Convert.ToInt32(item), Text = item.ToString() };
+            }
+        }
+    }
+}
