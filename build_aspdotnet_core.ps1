f1d4404160fa6c3d13b3e184aec0394b927343bc
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-21 14:20:38 -0500

    fixed build number

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -1,7 +1,4 @@
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
 dotnet restore .\ca_proto
 
 dotnet test .\ca_proto\ca_proto_tests
@@ -10,5 +7,5 @@ dotnet build --configuration Release .\ca_proto\ca_proto
 dotnet build --configuration Release .\ca_proto\ca_service
 
 $release = Join-Path $pwd release
-dotnet publish .\ca_proto\ca_proto -c Release -o $release --version-suffix=$revision
-dotnet publish .\ca_proto\ca_service -c Release -o $release --version-suffix=$revision
+dotnet publish .\ca_proto\ca_proto -c Release -o $release
+dotnet publish .\ca_proto\ca_service -c Release -o $release
