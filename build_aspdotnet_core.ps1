7ff4bdc250b409deda391904e3bf5a79e75f0cef
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-21 14:28:05 -0500

    split release folder for web and service projects

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -6,6 +6,5 @@ dotnet test .\ca_proto\ca_proto_tests
 dotnet build --configuration Release .\ca_proto\ca_proto
 dotnet build --configuration Release .\ca_proto\ca_service
 
-$release = Join-Path $pwd release
-dotnet publish .\ca_proto\ca_proto -c Release -o $release
-dotnet publish .\ca_proto\ca_service -c Release -o $release
+dotnet publish .\ca_proto\ca_proto -c Release -o .\release\ca_proto
+dotnet publish .\ca_proto\ca_service -c Release -o .\release\ca_service
