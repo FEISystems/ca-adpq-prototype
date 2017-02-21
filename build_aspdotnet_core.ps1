d22cad32fb8b322285d9d406c2e4b6e1a5e6732d
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-21 14:35:34 -0500

    rearranged for project dependencies

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -4,7 +4,6 @@ dotnet restore .\ca_proto
 dotnet test .\ca_proto\ca_proto_tests
 
 dotnet build --configuration Release .\ca_proto\ca_proto
-dotnet build --configuration Release .\ca_proto\ca_service
 
-dotnet publish .\ca_proto\ca_proto -c Release -o .\release\ca_proto
-dotnet publish .\ca_proto\ca_service -c Release -o .\release\ca_service
+$release = Join-Path $pwd release
+dotnet publish .\ca_proto\ca_proto -c Release -o $release
