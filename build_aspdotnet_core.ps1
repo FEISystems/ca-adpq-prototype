17a61fa67a484ca65c72e3e002dfe55e00ee557f
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-21 16:23:00 -0500

    added --configuration and --no-dependencies

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -1,9 +1,13 @@
 
-dotnet restore .\ca_proto
+dotnet restore .\ca_proto\ca_service
+dotnet restore .\ca_proto\ca_proto_tests
+dotnet restore .\ca_proto\ca_proto
+
+dotnet build .\ca_proto\ca_service --configuration Release --no-dependencies
+dotnet build .\ca_proto\ca_proto_tests --configuration Release --no-dependencies
+dotnet build .\ca_proto\ca_proto --configuration Release --no-dependencies
 
 dotnet test .\ca_proto\ca_proto_tests
-
-dotnet build .\ca_proto\ca_proto
 
 $release = Join-Path $pwd release
 dotnet publish .\ca_proto\ca_proto -c Release -o $release
