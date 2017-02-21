4dd746fb95ca6066e4a7a53d27918595ae47b8dd
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-21 15:10:44 -0500

    simplify build configuration

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -3,7 +3,7 @@ dotnet restore .\ca_proto
 
 dotnet test .\ca_proto\ca_proto_tests
 
-dotnet build --configuration Release --runtime ubuntu.16.10-x64 .\ca_proto\ca_proto
+dotnet build .\ca_proto\ca_proto
 
 $release = Join-Path $pwd release
 dotnet publish .\ca_proto\ca_proto -c Release -o $release
