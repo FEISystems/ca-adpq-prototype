73d0f7a60de8c66ff1487b4ca66b45e77fe77546
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-22 15:53:00 -0500

    trying to get the damn publishOptions to copy

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -5,8 +5,8 @@ dotnet test .\ca_proto\ca_proto_tests  --configuration Debug
 
 dotnet build .\ca_proto\ca_proto --configuration Debug
 
-dotnet publish .\ca_proto\ca_proto\project.json -o .\release --configuration Debug
+dotnet publish .\ca_proto\ca_proto\project.json --output .\release --configuration Debug
 
-copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
-copy-item .\ca_proto\ca_proto\wwwroot .\release\wwwroot -recurse
-copy-item .\ca_proto\ca_proto\Views .\release\Views -recurse
+#copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
+#copy-item .\ca_proto\ca_proto\wwwroot .\release\wwwroot -recurse
+#copy-item .\ca_proto\ca_proto\Views .\release\Views -recurse
