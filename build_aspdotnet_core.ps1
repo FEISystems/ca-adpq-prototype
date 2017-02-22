e0ffdd3b7331a02753d81c212052ce7a0300b098
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-22 13:39:30 -0500

    added manual copy of appSettings.json

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -6,3 +6,5 @@ dotnet test .\ca_proto\ca_proto_tests  --configuration Debug
 dotnet build .\ca_proto\ca_proto --configuration Debug
 
 dotnet publish .\ca_proto\ca_proto\project.json -o .\release --configuration Debug
+
+copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
