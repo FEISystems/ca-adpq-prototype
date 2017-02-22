6a5564fac6583d216063f865c51c131ca788e194
Luis Najera <luis.najera@feisystems.com>
2017-02-22 12:08:57 -0500

    Added serach results view. Added initial left menu changes.

---
+++ ca_proto/ca_proto/wwwroot/app/views/shared/layouts/aside-layout/aside.layout.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+    var module = angular.module("caWebApp");
+
+    module.directive('asideLayout', function() {
+        return {
+            templateUrl: "app/views/shared/layouts/aside-layout/aside.layout.html",
+            transclude : {
+                "aside-panel": 'asidePanel',
+                "body-panel": 'bodyPanel'
+            }
+        }
+        
+    })
+}())
