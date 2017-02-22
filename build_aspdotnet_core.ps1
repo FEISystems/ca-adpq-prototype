1a4d6c2bd5da9565570248070908a3e76ef0d29a
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-22 07:49:41 -0500

    tweak on project.json path

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -3,6 +3,6 @@ dotnet restore .\ca_proto
 
 dotnet test .\ca_proto\ca_proto_tests
 
-dotnet build .\ca_proto --configuration Release
+dotnet build .\ca_proto\ca_proto --configuration Release
 
-dotnet publish .\ca_proto\ca_proto\project.json --output $(build.stagingDirectory) --configuration Release
+dotnet publish .\ca_proto\ca_proto\project.json -o .\release -c Release
