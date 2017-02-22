35e51df6abac0550eb145dc38372ed17fedfc030
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-22 16:25:12 -0500

    added manual copies back

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -7,6 +7,6 @@ dotnet build .\ca_proto\ca_proto --configuration Debug
 
 dotnet publish .\ca_proto\ca_proto\project.json --output .\release --configuration Debug
 
-#copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
-#copy-item .\ca_proto\ca_proto\wwwroot .\release\wwwroot -recurse
-#copy-item .\ca_proto\ca_proto\Views .\release\Views -recurse
+copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
+copy-item .\ca_proto\ca_proto\wwwroot .\release\wwwroot -recurse
+copy-item .\ca_proto\ca_proto\Views .\release\Views -recurse
