2cc95a72ca612a1c04520d3a25658c07e2712a29
Ryan Chadwick <ryan.chadwick@feisystems.com>
2017-02-22 13:54:29 -0500

    publish is still not copying all items

--- build_aspdotnet_core.ps1
+++ build_aspdotnet_core.ps1
@@ -1,10 +1,10 @@
 
 dotnet restore .\ca_proto
 
-dotnet test .\ca_proto\ca_proto_tests  --configuration Debug
+dotnet test .\ca_proto\ca_proto_tests
 
-dotnet build .\ca_proto\ca_proto --configuration Debug
+dotnet build .\ca_proto\ca_proto
 
-dotnet publish .\ca_proto\ca_proto\project.json -o .\release --configuration Debug
+dotnet publish .\ca_proto\ca_proto\project.json -o .\release
 
-copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
+#copy-item .\ca_proto\ca_proto\appSettings.json .\release\appSettings.json
