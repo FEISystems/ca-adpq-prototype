7e96faab8d7c2b58f56ef556cb153f6eff30d7c3
David.lorek <David.lorek@DAVIDLOREKLT>
2017-02-23 18:45:48 -0500

    Started work on reports

--- ca_proto/ca_proto/wwwroot/app/areas/admin/reports/reports.js
+++ ca_proto/ca_proto/wwwroot/app/areas/admin/reports/reports.js
@@ -5,14 +5,73 @@
     var controller = function ($scope, $location) {
         var model = this;
         model.provider = {};
-        model.title = "All Reports";
+        model.title = "Reports";
+        model.tab = 1;
+        model.height = 400;
+        model.width = 700;
+
+        model.pieChart = function (context, height, width) {
+            var chart = this;
+            chart.context = context;
+            chart.height = height;
+            chart.width = width;
+            chart.radius = Math.floor(Math.min(height, width) / 2);
+            chart.centerX = Math.floor(width / 2);
+            chart.centerY = Math.floor(height / 2);
+
+            chart.fullSweep = 2.0 * Math.PI;
+            chart.zero = -0.5 * Math.PI;
+
+
+            chart.drawSlice = function (startPercent, sweepPercent, color) {
+                chart.context.beginPath();
+                var o = { a: chart.centerX, b: chart.centerY, c: chart.radius, d: chart.zero + startPercent * chart.fullSweep, e: chart.zero + sweepPercent * chart.fullSweep };
+                chart.context.arc(chart.centerX, chart.centerY, chart.radius, chart.zero + startPercent * chart.fullSweep, chart.zero + sweepPercent * chart.fullSweep);
+                chart.context.lineTo(chart.centerX, chart.centerY);
+                chart.context.stroke();
+                chart.context.fillStyle = color;
+                chart.context.fill();
+            };
+
+            chart.clear = function () {
+                chart.context.clearRect(0, 0, chart.width, chart.height);
+            };
+
+            return chart;
+        }
+
+        model.showFilter = function() {
+            model.tab = 1;
+        }
+
+        model.showExpendituresByProductType = function() {
+            model.tab = 2;
+            var canvas = document.getElementById("productTypeCanvas");
+            var context = canvas.getContext("2d");
+            var pieChart = model.pieChart(context, model.height, model.width);
+            pieChart.drawSlice(0, 1.0 / 3.0, "red");
+            pieChart.drawSlice(1.0 / 3.0, 2.0 / 3.0, "blue");
+            pieChart.drawSlice(2.0 / 3.0, 1, "green");
+        }
+
+        model.showExpendituresByContractor = function() {
+            model.tab = 3;
+        }
+
+        model.showPurchasesByAccount = function() {
+            model.tab = 4;
+        }
+
+        model.showRawData = function() {
+            model.tab = 5;
+        }
 
     };
 
     module.component("reports", {
         templateUrl: "app/areas/admin/reports/reports.html",
         controllerAs: "model",
-        controller: ["$scope", "$location", controller]
+        controller: ["$scope", "$location", "messageService", "inventoryService", controller]
 
     });
 }())
---
+++ ca_proto/ca_proto/wwwroot/app/services/reportService.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    var reportService = function (messageService, $http) {
+
+        //var fetchProducts = function (start, count, orderByColumn, orderAscending, filter) {
+        //    var postData = { start, count, orderByColumn, orderAscending, filter };
+        //    $http.post("/api/inventory/query", postData)
+        //        .success(function (response) {
+        //            messageService.publish('querySuccess', response);
+        //        })
+        //        .error(function (response) {
+        //            messageService.publish('queryFailure', response);
+        //        });
+        //}
+
+        return {
+        //    fetchProducts: fetchProducts,
+        };
+    }
+
+    var module = angular.module("caWebApp");
+    module.factory("reportService", reportService);
+}())
