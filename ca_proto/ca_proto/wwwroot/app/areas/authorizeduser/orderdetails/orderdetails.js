6707e128c1faaea1215d1925cbde219e2fbf2f97
Luis Najera <luis.najera@feisystems.com>
2017-02-24 01:34:47 -0500

    Added initial functionality for placing and viewing orders. Added extra product images. Linked image file names to the csv file.

--- ca_proto/ca_proto/wwwroot/app/areas/authorizeduser/orderdetails/orderdetails.js
+++ ca_proto/ca_proto/wwwroot/app/areas/authorizeduser/orderdetails/orderdetails.js
@@ -2,17 +2,40 @@
     "use strict";
     var module = angular.module("caWebApp");
 
-    var controller = function ($scope, $location) {
+    var controller = function ($scope, $location, messageService, orderService, $rootScope) {
         var model = this;
         model.provider = {};
         model.title = "Order Details";
+        this.$routerOnActivate = function (next, previous) {
+            model.orderId = parseInt(next.params.id);
+
+
+            model.getOrder = function () {                
+                orderService.getOrder(model.orderId);
+            };
+
+            model.getOrder();
+
+
+
+            messageService.subscribe("getOrderSuccess", function (response) {
+                model.order = response;
+            })
+            messageService.subscribe("getOrderFailure", function (response) {
+                model.order = {};
+            })
+
+
+        }
+
+
 
     };
 
     module.component("orderDetails", {
         templateUrl: "app/areas/authorizeduser/orderdetails/orderdetails.html",
         controllerAs: "model",
-        controller: ["$scope", "$location", controller]
+        controller: ["$scope", "$location", "messageService", "orderService", "$rootScope", controller]
 
     });
 }())
