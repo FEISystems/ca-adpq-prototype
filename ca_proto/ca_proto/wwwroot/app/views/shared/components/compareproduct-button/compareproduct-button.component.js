4ef0aff1af7334a8a6c1377199f3452b4ff652f9
Luis Najera <luis.najera@feisystems.com>
2017-02-27 00:51:58 -0500

    Fixed issues related to Compare List not
    consistently working. Remove functionality now
    works from within the Compare List page. Added
    and removed fields based on updated requirements. Persisted compare list using session storage. #20

--- ca_proto/ca_proto/wwwroot/app/views/shared/components/compareproduct-button/compareproduct-button.component.js
+++ ca_proto/ca_proto/wwwroot/app/views/shared/components/compareproduct-button/compareproduct-button.component.js
@@ -2,44 +2,49 @@
     "use strict";
     var module = angular.module("caWebApp");
 
-    var controller = function ($scope, shoppingCartService, $element, $rootScope) {
+    var controller = function ($scope, shoppingCartService, $element, $rootScope, $sessionStorage) {        
         var model = this;
-        model.productId = $scope.productId
 
-        if (!$rootScope.compareList) {
-            $rootScope.compareList = []
-        }
+            model.productId = $scope.productId;
 
-        $scope.addToCompareList = function(productId) {
-            
-        }
-
-        if (model.productId in $rootScope.compareList)  {                    
-            $element.find("input").prop('checked', true);
-        }
-
-        $element.find("input").on("change", function(){
-            var checkbox = this;
-            if ($(checkbox).is(":checked")) {
-                if (!(model.productId in $rootScope.compareList) && $rootScope.compareList.length < 4) {                    
-                    $rootScope.compareList.push(model.productId);
-                } else {                                     
-                    $element.find("input").prop('checked', false);
-                    alert("Only 4 items can be compared at a time.");
-                }
-                
-            } else {
-                $rootScope.compareList = jQuery.grep($rootScope.compareList, function(value) {
-                                            return value != model.productId;
-                                        });;
+            if (!$sessionStorage.compareList) {
+                $sessionStorage.compareList = [];
             }
-        });
+
+            $scope.addToCompareList = function(productId) {
+                
+            }
+
+            if ($sessionStorage.compareList) {                 
+                if ($.inArray( model.productId, $sessionStorage.compareList ) > -1)  {                    
+                    $($element).find("input").prop('checked', true);
+                }
+            }
+        
+            $($element).find("input").on("change", function(){
+
+                var checkbox = this;
+
+                if ($(checkbox).is(":checked")) {
+                    if (!($.inArray( model.productId, $sessionStorage.compareList ) > -1) && $sessionStorage.compareList.length < 4) {                    
+                        $sessionStorage.compareList.push(model.productId);
+                    } else {                                     
+                        $($element).find("input").prop('checked', false);
+                        alert("Only 4 items can be compared at a time.");
+                    }
+                    
+                } else {
+                    $sessionStorage.compareList = jQuery.grep($sessionStorage.compareList, function(value) {
+                                                    return value != model.productId;
+                                                });;
+                }
+            });
     };
 
     module.directive("compareProductButton", function () {
         return {
             template: '<div><input type="checkbox" id="compareItem{{$id}}"><label for="compareItem{{$id}}">Compare</label></div>',
-            controller: ["$scope", "shoppingCartService", "$element", "$rootScope", controller],
+            controller: ["$scope", "shoppingCartService", "$element", "$rootScope", "$sessionStorage", controller],
             replace: true,
             scope: { productId: '=' }
         }
