4c9578e5d7df5f63c9e767a53fbf068cfb3987ac
Luis Najera <luis.najera@feisystems.com>
2017-02-26 22:43:19 -0500

    Fixed issue with categories sections not returning all related products. Refactored categories front- end for maintainability.

--- ca_proto/ca_proto/wwwroot/app/app.js
+++ ca_proto/ca_proto/wwwroot/app/app.js
@@ -39,53 +39,13 @@
             { path: '/product/details/:id', component: 'productDetails', name: 'Product Details'},
             { path: '/products/searchresults', component: 'searchResults', name: 'Search Results'},
 
-            // products
-            // // Software
-            { path: '/products/software/allsoftware', component: 'allSoftware', name: 'All Software' },   
-
-            // // Hardware
-            // // // Desktops   
-            { path: '/products/hardware/alldesktops', component: 'allDesktops', name: 'All Desktops' },     
-            { path: '/products/hardware/standarddesktops', component: 'standardDesktops', name: 'Standard Desktops' },
-            { path: '/products/hardware/powerdesktops', component: 'powerDesktops', name: 'Power Desktops' },
-            { path: '/products/hardware/workstations', component: 'workstations', name: 'Workstations' },
-            { path: '/products/hardware/thinclientdesktops', component: 'thinClientDesktops', name: 'Thin Client Desktops' },
-            { path: '/products/hardware/allinonedesktops', component: 'allInOneDesktops', name: 'All in One Desktops' },
-            // // // Laptops
-            { path: '/products/hardware/alllaptops', component: 'allLaptops', name: 'All Laptops' },  
-            { path: '/products/hardware/standardlaptops', component: 'standardLaptops', name: 'Standard Laptops' },  
-            { path: '/products/hardware/powerlaptops', component: 'powerLaptops', name: 'Power Laptops' },  
-            { path: '/products/hardware/ultralightlaptops', component: 'ultralightLaptops', name: 'Ultralight Laptops' },  
-            { path: '/products/hardware/mobileworkstations', component: 'mobileWorkstations', name: 'Mobile Workstations' },     
-            // // // Accessories
-            { path: '/products/hardware/allaccessories', component: 'allAccessories', name: 'All Accessories' }, 
-            { path: '/products/hardware/desktopaccessories', component: 'desktopAccessories', name: 'Desktop Accessories' }, 
-            { path: '/products/hardware/laptopaccessories', component: 'laptopAccessories', name: 'Laptop Accessories' }, 
-            { path: '/products/hardware/monitors', component: 'monitors', name: 'Monitors' }, 
-            { path: '/products/hardware/miscellaneous', component: 'miscellaneous', name: 'Miscellaneous' },  
-
-            // // Services
-            // // // Desktops   
-            { path: '/products/services/alldesktopservices', component: 'allDesktopServices', name: 'All Desktop Services' },     
-            { path: '/products/services/standarddesktopservices', component: 'standardDesktopServices', name: 'Standard Desktop Services' },
-            { path: '/products/services/powerdesktopservices', component: 'powerDesktopServices', name: 'Power Desktop Services' },
-            { path: '/products/services/workstationservices', component: 'workstationServices', name: 'Workstation Services' },
-            { path: '/products/services/thinclientdesktopservices', component: 'thinClientDesktopServices', name: 'Thin Client Desktop Services' },
-            { path: '/products/services/allinonedesktopservices', component: 'allInOneDesktopServices', name: 'All in One Desktop Services' },
-            // // // Laptops
-            { path: '/products/services/alllaptopservices', component: 'allLaptopServices', name: 'All Laptop Services' },  
-            { path: '/products/services/standardlaptopservices', component: 'standardLaptopServices', name: 'Standard Laptop Services' },  
-            { path: '/products/services/powerlaptopservices', component: 'powerLaptopServices', name: 'Power Laptop Services' },  
-            { path: '/products/services/ultralightlaptopservices', component: 'ultralightLaptopServices', name: 'Ultralight Laptop Services' },  
-            { path: '/products/services/mobileworkstationservices', component: 'mobileWorkstationServices', name: 'Mobile Workstation Services' }, 
-            // // // Value Added Services
-            { path: '/products/services/allvalueaddedservices', component: 'allValueAddedServices', name: 'All VAS' },    
-            { path: '/products/services/desktopvalueaddedservices', component: 'desktopValueAddedServices', name: 'Desktop VAS' },    
-            { path: '/products/services/laptopvalueaddedservices', component: 'laptopValueAddedServices', name: 'Laptop VAS' },    
-
+            //Category Lists
+            { path: '/products/hardware/:category', component: 'categoryList', name: 'Hardware List' },  
+            { path: '/products/services/:category', component: 'categoryList', name: 'Services List' },              
+            { path: '/products/software/:category', component: 'categoryList', name: 'Software List' },  
             
             // Admin
-            { path: '/admin/home', component: 'adminHome', name: 'Admin Home', requiresAuthentication : true },
+            { path: '/admin/home', component: 'adminHome', name: 'Admin Home'},
             { path: '/admin/login', component: 'adminLogin', name: 'Admin Login'}, //Login Testing Only
             { path: '/admin/adminuser', component: 'adminUser', name: 'Admin User'},
             { path: '/admin/adminusers', component: 'adminUsers', name: 'Admin Users'},
