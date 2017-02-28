1cbafc6db5a8de7c283bf02972b988c4697d1761
jonas.bush <jonas.bush@feisystems.com>
2017-02-28 13:00:13 -0700

    Remove services from home controller

--- ca_proto/ca_proto/Controllers/HomeController.cs
+++ ca_proto/ca_proto/Controllers/HomeController.cs
@@ -1,40 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ca_proto.Models;
-using ca_proto.Helpers;
-using Microsoft.AspNetCore.Mvc;
-using ca_service.Interfaces;
-using ca_service.Entities;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ca_proto.Controllers
 {
-    /// <summary>
-    /// A controller intercepts the incoming browser request and returns
-    /// an HTML view (.cshtml file) or any other type of data.
-    /// </summary>
     public class HomeController : Controller
     {
-        public IUserService userService;
-        public IInventoryService inventoryService;
 
-        public HomeController(IUserService userService, IInventoryService inventoryService)
+        public HomeController()
         {
-            this.userService = userService;
-            this.inventoryService = inventoryService;
         }
 
         public IActionResult Index()
         {
-            //var products = inventoryService.QuickSearch(new[] { "test" });
-            //return View(products?.Count() ?? 0);
             return View();
-        }
-
-        public IActionResult About()
-        {
-            return View(this.userService.GetUsers());
         }
     }
 }
