4176c8a3eb617ab14fe208b8b55acc3a21485b86
christianhfei <Christian.Heyd@fei.local>
2017-03-01 10:49:22 -0500

    Began adding api calls for #151

--- ca_proto/ca_proto/wwwroot/app/areas/authorizeduser/orderconfirmation/orderconfirmation.js
+++ ca_proto/ca_proto/wwwroot/app/areas/authorizeduser/orderconfirmation/orderconfirmation.js
@@ -2,21 +2,33 @@
     "use strict";
     var module = angular.module("caWebApp");
 
-    var controller = function ($scope, $location) {
+    var controller = function ($scope, $location, messageService, orderService) {
         var model = this;
         model.provider = {};
         model.title = "Order Confirmation";
 
+        orderService.getOrdersByUserId();
+
+        model.findOrderNumber = function (orderNumberId) {
+        }
+
         $scope.continueShopping = function() {
             $location.path("home");
         }
+
+        messageService.subscribe("getOrdersByUserIdSuccess", function (response) {
+            model.orderNumbers = response;
+            for (var i = 0; i < response.length; i++) {
+                response[i] = model.findOrderNumber(response[i]);
+            }
+        })
 
     };
 
     module.component("orderConfirmation", {
         templateUrl: "app/areas/authorizeduser/orderconfirmation/orderconfirmation.html",
         controllerAs: "model",
-        controller: ["$scope", "$location", controller]
+        controller: ["$scope", "$location", "messageService", "orderService", controller]
 
     });
 }())
