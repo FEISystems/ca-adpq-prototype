1a42c20bd3c87bb9d9bf2c7d94225c7e5623589e
jonas.bush <jonas.bush@feisystems.com>
2017-03-01 10:57:49 -0700

    #175 - add validation for adding to cart

--- ca_proto/ca_proto/wwwroot/app/views/shared/components/addtocart-qty-button/addtocart-qty-button.component.js
+++ ca_proto/ca_proto/wwwroot/app/views/shared/components/addtocart-qty-button/addtocart-qty-button.component.js
@@ -6,11 +6,20 @@
         var model = this;
         var product = $scope.product;
         model.productId = $scope.product.Id;
-        model.qty = "";
+        model.qty = 1;
+
+        model.isInvalidQuantity = function () {
+            if (model.qty == null || isNaN(model.qty))
+                return true;
+
+            var theQuantity = Number(model.qty);
+
+            return theQuantity <= 0;
+        }
 
 
         $scope.addToCart = function () {
-            shoppingCartService.addProductToCart({"ProductId" : model.productId, "Quantity" : parseInt(model.qty)});
+            shoppingCartService.addProductToCart({"ProductId" : model.productId, "Quantity" : parseInt(model.qty) });
         }
 
 
